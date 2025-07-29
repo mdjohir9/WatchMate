99d30d9bbf3989bd062a8b2a9cd158a241ee61b7
Md Johirull Islam <143317864+mdjohircw@users.noreply.github.com>
2025-07-30 02:17:46 +0600

    home project setup done with migration changes

---
+++ Migrations/20250729200658_db_upload_on_home_pc.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchMate_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class db_upload_on_home_pc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
